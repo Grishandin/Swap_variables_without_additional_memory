e0f9f23daca5d785d6df5a37524628eea365a875
Rofling <55977764+Rofling@users.noreply.github.com>
2019-12-11 20:38:12 +0300

    added 2nd method
    added 2nd method

--- src/Grishandin/Swap_two_variables.java
+++ src/Grishandin/Swap_two_variables.java
@@ -12,9 +12,13 @@ public class Swap_two_variables {
         System.out.print("Type b:");
         int b =scan.nextInt();
         a=b+a;
-        b=b-a+b;
-        a=(a+b)/2;
-        b=(2*a-2*b)/2;
+        b=a-b; //b=a+b-b=a
+        a=a-b; //a=a+b-a=b
+//        2nd way
+//        a=b+a;
+//        b=b-a+b; //b=b-b-a+b=b-a
+//        a=(a+b)/2; //a=(a+b+b-a)/2=2b/2=b
+//        b=(2*a-2*b)/2; //b=(2b-2b+2a)/2=2a/a=a
         System.out.print("a: "+ a+" b: "+ b);
     }
 }
