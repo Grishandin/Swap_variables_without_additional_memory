9b9d07d1da24646ab989f9eee2c08d85dfc34164
Rofling <55977764+Rofling@users.noreply.github.com>
2020-01-17 15:30:16 +0300

    Update Swap_two_variables.java

--- src/Grishandin/Swap_two_variables.java
+++ src/Grishandin/Swap_two_variables.java
@@ -18,7 +18,7 @@ public class Swap_two_variables {
 //        a=b+a;
 //        b=b-a+b; //b=b-b-a+b=b-a
 //        a=(a+b)/2; //a=(a+b+b-a)/2=2b/2=b
-//        b=(2*a-2*b)/2; //b=(2b-2b+2a)/2=2a/a=a
+//        b=a-b; //b=b-b+a=a
         System.out.print("a: "+ a+" b: "+ b);
     }
 }
